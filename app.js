46e697d371655450e9f2c18e3baa61e08148ba01
Paul Maserrat <pmaserrat@gmail.com>
2015-09-21 20:20:27 -0400

    app.js fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 app.get('/', function (req, res) {
   res.render('index',
-  { title : 'Home' }
+  { title : "Paul Maserrat's Awesome Site" }
   )
 })
 app.listen(3000)
