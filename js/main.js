625e0450ad8075198094965e116f10b70af47ca7
Paul Maserrat <pmaserrat@gmail.com>
2016-06-24 23:28:24 -0400

    adding some bootstrap

--- js/main.js
+++ js/main.js
@@ -40,13 +40,17 @@ app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   console.log("Page Controller reporting for duty.");
 
-  // Activates the Carousel
-  $('.carousel').carousel({
-    interval: 5000
-  });
-
   // Activates Tooltips for Social Links
   $('.tooltip-social').tooltip({
     selector: "a[data-toggle=tooltip]"
   })
+  
+	$('.nav a').on('click', function(){
+		$('.navbar-collapse').collapse('hide');
+	});
+  	$(document).on('click',function(){
+		$('.navbar-collapse').collapse('hide');
+	});
 });
+
+
