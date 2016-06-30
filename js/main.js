3b66696cdcc9fe9ec1e1403a5dea51c102808cf3
Paul Maserrat <pmaserrat@gmail.com>
2016-06-29 22:18:24 -0400

    parallax and images

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ app.config(['$routeProvider', function ($routeProvider) {
     .when("/index.html", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
     .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
+    .when("/family", {templateUrl: "partials/family.html", controller: "PageCtrl"})
     .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
     .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
     .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
@@ -51,6 +51,64 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
   	$(document).on('click',function(){
 		$('.navbar-collapse').collapse('hide');
 	});
+  	
+  	/**
+  	 * Author: Heather Corey
+  	 * jQuery Simple Parallax Plugin
+  	 *
+  	 */
+  	 
+  	(function($) {
+  	 
+  	    $.fn.parallax = function(options) {
+  	 
+  	        var windowHeight = $(window).height();
+  	 
+  	        // Establish default settings
+  	        var settings = $.extend({
+  	            speed        : 0.15
+  	        }, options);
+  	 
+  	        // Iterate over each object in collection
+  	        return this.each( function() {
+  	 
+  	        	// Save a reference to the element
+  	        	var $this = $(this);
+  	 
+  	        	// Set up Scroll Handler
+  	        	$(document).scroll(function(){
+  	 
+  	    		        var scrollTop = $(window).scrollTop();
+  	            	        var offset = $this.offset().top;
+  	            	        var height = $this.outerHeight();
+  	 
+  	    		// Check if above or below viewport
+  				if (offset + height <= scrollTop || offset >= scrollTop + windowHeight) {
+  					return;
+  				}
+  	 
+  				var yBgPosition = Math.round((offset - scrollTop) * settings.speed);
+  	 
+  	                 // Apply the Y Background Position to Set the Parallax Effect
+  	    			$this.css('background-position', 'center ' + yBgPosition + 'px');
+  	                
+  	        	});
+  	        });
+  	    }
+  	}(jQuery));
+
+  	$('.bg-1,.bg-3').parallax({
+  		speed :	0.15
+  	});
+
+  	$('.bg-2').parallax({
+  		speed :	0.25
+  	});
+  	
+  	
+  	
 });
+
+
 
 
